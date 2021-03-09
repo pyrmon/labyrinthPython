e753ef620f62ff746ffd3d74e1eeaeb26749f39b
Florian Schickel <florian@schickel.me>
2021-03-09 20:15:00 +0100

    implemented the step functionality. Sadly I didn't get to the logic part yet

--- florian/definitionsLabyrinth.py
+++ florian/definitionsLabyrinth.py
@@ -2,18 +2,111 @@ import os
 
 #beginning of the S class
 class S:
-    def __init__(self, location):
+    def __init__(self, location, direction):
         self.location = location
+        self.direction = direction
     
-    #here I want to scout the area and base decisions based on the Ss location
-    def getWhiteSpace(s,labyrinth):
+    #here I want to scout the area and base decisions based on the Ss location. 0 = up, 1 = right, 2 = down, 3 = up
+    def directionalDict(self,labyrinth):
         directions = {}
+        yValue = self.location["yValue"]
+        xValue = self.location["xValue"]
+        directions[0] = labyrinth[yValue - 1 ][xValue]
+        directions[1] = labyrinth[yValue][xValue + 1]
+        directions[2] = labyrinth[yValue + 1 ][xValue]
+        directions[3] = labyrinth[yValue][xValue - 1]
+        return directions
+    
+    def goOneStepForward(self,labyrinth):
+        if self.direction == 0:
+            currentX = self.location["xValue"]
+            currentY = self.location["yValue"]
+            newStringY = ''
+            newStringYMinusOne = ''
+            counter = 0
+            for each in labyrinth[currentY]:
+                if counter == currentX:
+                    newStringY += ' '
+                else:
+                    newStringY += each
+                counter += 1
+            counter = 0
+            for each in labyrinth[currentY-1]:
+                if counter == currentX:
+                    newStringYMinusOne += 'S'
+                else:
+                    newStringYMinusOne += each
+                counter += 1
+            labyrinth[currentY] = newStringY
+            labyrinth[currentY-1] = newStringYMinusOne
+            self.location["yValue"] = currentY - 1
+            return labyrinth
+        
+        if self.direction == 1:
+            currentX = self.location["xValue"]
+            currentY = self.location["yValue"]
+            newStringY = ''
+            counter = 0
+            for each in labyrinth[currentY]:
+                if counter == currentX:
+                    newStringY += ' '
+                elif counter == currentX + 1:
+                    newStringY += 'S'
+                else:
+                    newStringY += each
+                counter += 1
+            labyrinth[currentY] = newStringY
+            self.location["xValue"] = currentY + 1
+            return labyrinth
+        
+        if self.direction == 2:
+            currentX = self.location["xValue"]
+            currentY = self.location["yValue"]
+            newStringY = ''
+            newStringYPlusOne = ''
+            counter = 0
+            for each in labyrinth[currentY]:
+                if counter == currentX:
+                    newStringY += ' '
+                else:
+                    newStringY += each
+                counter += 1
+            counter = 0
+            for each in labyrinth[currentY+1]:
+                if counter == currentX:
+                    newStringYPlusOne += 'S'
+                else:
+                    newStringYPlusOne += each
+                counter += 1
+            labyrinth[currentY] = newStringY
+            labyrinth[currentY-1] = newStringYPlusOne
+            self.location["yValue"] = currentY + 1
+            return labyrinth
+        
+        if self.direction == 3:
+            currentX = self.location["xValue"]
+            currentY = self.location["yValue"]
+            newStringY = ''
+            counter = 0
+            for each in labyrinth[currentY]:
+                if counter == currentX:
+                    newStringY += ' '
+                elif counter == currentX - 1:
+                    newStringY += 'S'
+                else:
+                    newStringY += each
+                counter += 1
+            labyrinth[currentY] = newStringY
+            self.location["xValue"] = currentY + 1
+            return labyrinth
+
+
 
 # here I am reading out the labyrinthfile
 def createLabyrinthDic():
     #fileDir = os.path.dirname(os.path.realpath('__file__'))
     #filename = os.path.join(fileDir, 'labyrinthPython/labyrinth.map')
-    filename = "labyrinth.map" #Max: added this line to simply read map file in working dir
+    filename = "labyrinth.map" 
     f = open(filename,"r")
     labyrinth = {}
     counter = 0
@@ -26,7 +119,7 @@ def createLabyrinthDic():
         counter += 1
     return labyrinth
         
-#Max: fixed output of location of S by moving some indents around
+#getting S location
 def getS(labyrinth):
     location = {}
     yValue = 0
--- florian/testPage.py
+++ florian/testPage.py
@@ -8,5 +8,14 @@ for x in range(len(lab)):
     print(lab[x])
 
 #trying to get my Ss location
-print(dl.getS(lab))
+location = dl.getS(lab)
+Spieler = dl.S(location,3) 
+
+lab = Spieler.goOneStepForward(lab)
+
+print("\n\n\n\n\n")
+
+for x in range(len(lab)):
+    print(lab[x])
+    
 
