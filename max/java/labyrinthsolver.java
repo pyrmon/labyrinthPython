adc5fab0ae6228356bd356bc39dd41d357901466
MasterMax13124 <mastermax13124@gmail.com>
2021-03-10 12:55:36 +0100

    Cleaned up my folder, moved gitignores and added more java code

--- max/java/labyrinthsolver.java
+++
@@ -1,6 +0,0 @@
-class labyrinthsolver{
-	public static void main(String[] args){
-		labyrinth lab = new labyrinth("labyrinth.txt");
-		lab.printLabyrinth();
-	}
-}
--- max/python/labyrinth.py
+++
@@ -1,6 +0,0 @@
-class labyrinth:
-    labmap = []
-    def __init__(filename):
-        with open(filename) as f:
-            content = f.readlines()
-
